8780b7bf482b2877e0510b2dc0dedc7586fd9f3a
Jari Haavisto <jari.haavisto@buutti.com>
2026-02-02 12:43:11 +0200

    Use root as db location

--- src/db.py
+++ src/db.py
@@ -1,9 +1,11 @@
 from pathlib import Path
 import sqlite3
 
+BASE_DIR = Path(__file__).resolve().parents[1]
+DB_PATH = BASE_DIR / "database.db"
 
 def get_conn():
-    conn = sqlite3.connect(Path(__file__).parent / "database.db")
+    conn = sqlite3.connect(DB_PATH)
     conn.row_factory = sqlite3.Row
     return conn
 
