2950621dc28177ea98cb16d4f1be2db13aed849e
Jari Haavisto <jari.haavisto@buutti.com>
2026-02-02 12:04:04 +0200

    Add GET frontpage

--- src/app.py
+++ src/app.py
@@ -28,6 +28,10 @@ def send_status(self, status=200):
 
 
 class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"Niukka API")
     def do_POST(self):
         body = read_json(self)
         if body is None:
